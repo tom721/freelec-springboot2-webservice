96f193e67795b7cb7d532e6d181b0edf5824a919
tom721 <tom721@naver.com>
2021-02-24 12:01:55 +0900

    webMvcTest 유닛 테스트 에러 해결

--- src/test/java/com/springboot/example/helloController/web/HelloControllerTest.java
+++ src/test/java/com/springboot/example/helloController/web/HelloControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
+@MockBean(JpaMetamodelMappingContext.class)
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
